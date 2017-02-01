274f5088020c76be1346d12e8322c38c96548240
Felipe Plets <felipe.plets@menvia.com>
2017-02-01 18:45:21 -0200

    Add ESLint

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+## 0.0.1 / 2017.02.01
+
+##### Enhancements
+- First version
+- Add a node to get status from a Jenkins instance
+
+##### Dependencies
+- Add [jenkins 0.20.0](https://github.com/silas/node-jenkins/releases)
+
+##### Dev dependencies
+- Add [eslint 3.14.1](https://github.com/eslint/eslint/releases)
+- Add [eslint-config-google 0.7.1](https://github.com/google/eslint-config-google/releases)
+- Add [eslint-watch 2.1.14](https://github.com/rizowski/eslint-watch/releases)
--- status/status.js
+++ status/status.js
@@ -1,16 +1,25 @@
 module.exports = function(RED) {
+    /**
+     * Get status from a Jenkins instance
+     * @param {string} config - The Node-RED node configuration
+     */
     function getStatus(config) {
-        RED.nodes.createNode(this,config);
+        RED.nodes.createNode(this, config);
         this.url = config.url;
-        var node = this;
+        let node = this;
         this.on('input', function(msg) {
-            var jenkins = require('jenkins')({ baseUrl: node.url, crumbIssuer: true });
-            jenkins.info(function(err, data) {
+            let jenkins = require('jenkins');
+            let jenkinsConfig = {
+                baseUrl: node.url,
+                crumbIssuer: true,
+            };
+            let jenkinsInstance = jenkins(jenkinsConfig);
+            jenkinsInstance.info(function(err, data) {
                 if (err) throw err;
                 msg.payload = data;
                 node.send(msg);
             });
         });
     }
-    RED.nodes.registerType("jenkins-status", getStatus);
-}
+    RED.nodes.registerType('jenkins-status', getStatus);
+};
