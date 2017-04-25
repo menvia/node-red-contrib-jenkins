3170a899a97749346f988ba20568a341eb6f05f9
lethalpaga <lethalpaga@gmail.com>
2017-04-25 08:46:43 +0000

    Add options to list or build jobs

--- nodes/job.js
+++ nodes/job.js
@@ -12,29 +12,72 @@ module.exports = function(RED) {
         this.job = config.job;
         this.jobType = config.jobType;
         this.parameters = config.parameters;
+        this.action = config.action;
 
         let node = this;
         this.on('input', function(msg) {
             let jenkins_server = RED.nodes.getNode(node.server);
 
-            let job = RED.util.evaluateNodeProperty(node.job, node.jobType, node, msg);
-            jenkins_server.api.job.build({name: job, parameters: this.parameters}, function(err, data) {
-                if (err) {
-                    node.status({ 
-                                  fill: "red",
-                                  shape: "dot",
-                                  text: "Error running job" 
-                                });
-                    node.error("Error starting job " + this.job, err);
-                    return
-                }
+            if (msg.jenkins == undefined) msg.jenkins = {}
 
-                if (msg.payload.jenkins == undefined) msg.payload.jenkins = {}
-                msg.payload.jenkins.buildNumber = data
+            if (node.action == "build") {
+                let job = RED.util.evaluateNodeProperty(node.job, node.jobType, node, msg);
+                let parameters = formatParameters(node.parameters);
+                jenkins_server.api.job.build({name: job, parameters: parameters}, function(err, data) {
+                    if(!processError(node, err)) return;
+                    if (msg.jenkins.job == undefined) msg.jenkins.job = {}
+                    if (msg.jenkins.build == undefined) msg.jenkins.build = {}
 
-                node.send(msg);
-            });
+                    msg.jenkins.build.number = data;
+                    msg.jenkins.job.name = job;
+                    node.send(msg);
+                });
+            }
+            else if (node.action == "get") {
+                let job = RED.util.evaluateNodeProperty(node.job, node.jobType, node, msg);
+                jenkins_server.api.job.get({ name: job }, function(err, data) {
+                    if(!processError(node, err)) return;
+
+                    msg.jenkins.job = data;
+                    node.send(msg);
+                });
+            }
+            else if (node.action == "list") {
+                jenkins_server.api.job.list(function(err, data) {
+                    if(!processError(node, err)) return;
+
+                    msg.jenkins.jobs = data
+                    node.send(msg);
+                });
+            }
         });
     }
     RED.nodes.registerType('jenkins-job', job);
+
+    function formatParameters(parameters) {
+        let res = {}
+        for (var i = 0; i < parameters.length; i++) {
+            var parameter = parameters[i];
+            res[parameter.label] = parameter.value;
+        }
+        return res;
+    }
+
+    function processError(node, err) {
+        if (err) {
+            node.status({ 
+                fill: "red",
+                shape: "dot",
+                text: "Error running job" 
+            });
+            node.error("Error starting job: " + err, err);
+            return false;
+        }
+        else {
+            // Clear the error status
+            node.status({});
+        }
+
+        return true;
+    }
 };
